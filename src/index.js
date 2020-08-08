f94413fdaa7fb3627235b1274caa8c48a2bb2c55
akaplan492 <68351916+akaplan492@users.noreply.github.com>
2020-08-08 11:48:58 -0400

    Updated index.js
    Copied code from original codesandbox file.

---
+++ src/index.js
@@ -0,0 +1,106 @@
+//Show temperature of search city
+
+function displayWeatherCondition(response) {
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#descriptionData").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#windData").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let apiKey = "6afd4a55eb7aa4136dc45db8d1efb3c6";
+  let city = document.querySelector("#inputCity").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+let search = document.querySelector("#cityButton");
+search.addEventListener("click", handleSubmit);
+
+//Show temperature using geolocation
+function showPosition(position) {
+  let apiKey = "6afd4a55eb7aa4136dc45db8d1efb3c6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let currentLocationButton = document.querySelector("#locationButton");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+//Temperature Unit Conversion
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = temperatureElement.innerHTML;
+  temperature = Number(temperature);
+  temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = temperatureElement.innerHTML;
+  temperatureElement.innerHTML = Math.round(((temperature - 32) * 5) / 9);
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+//Current Date and Timelet currentDate = new Date();
+let currentDate = new Date();
+
+let h2 = document.querySelector("h2");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let day = days[currentDate.getDay()];
+let month = months[currentDate.getMonth()];
+let date = currentDate.getDate();
+let year = currentDate.getFullYear();
+let hours = currentDate.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = currentDate.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+h2.innerHTML = `${day}, ${month} ${date} ${year} ${hours}:${minutes}`;
